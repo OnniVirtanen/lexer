1e0e430d7c80f01e9201296e2d20e9cd4b875c09
Onni Virtanen <onni.virtanen5@gmail.com>
2024-07-11 21:58:22 +0300

    gitignore

--- lexer-rust/target/debug/deps/lexer_rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\onniv\source\github\lexer\lexer-rust\target\debug\deps\lexer_rust.exe: src\main.rs
-
-C:\Users\onniv\source\github\lexer\lexer-rust\target\debug\deps\lexer_rust.d: src\main.rs
-
-src\main.rs:
--- lexer-rust/target/debug/lexer-rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\onniv\source\github\lexer\lexer-rust\target\debug\lexer-rust.exe: C:\Users\onniv\source\github\lexer\lexer-rust\src\main.rs
